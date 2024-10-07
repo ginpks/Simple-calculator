8d5278c3bdb4b292d63ce7db00010aff944782dd
ginpks <ginhpark@gmail.com>
2024-10-06 23:18:01 -0400

    add duck

--- script.js
+++ script.js
@@ -1,59 +1,77 @@
 let buttons = document.querySelectorAll(".btn");
 let display = document.getElementById("display");
+const quack = new Audio("quack_5.mp3");
 
 buttons.forEach(btn => {
     btn.addEventListener("click", event => {
         const value = event.target.value;
         switch(value) {
             case "AC":
+                quack.play();
                 display.value = "";
                 break;
             case "⌫":
+                quack.play();
                 display.value = display.value.slice(0, -1);
                 break;
             case "1":
+                quack.play();
                 display.value += 1;
                 break;
             case "2":
+                quack.play();
                 display.value += 2;
                 break;
             case "3":
+                quack.play();
                 display.value += 3;
                 break;
             case "4":
+                quack.play();
                 display.value += 4;
                 break;
             case "5":
+                quack.play();
                 display.value += 5;
                 break;
             case "6":
+                quack.play();
                 display.value += 6;
                 break;
             case "7":
+                quack.play();
                 display.value += 7;
                 break;
             case "8":
+                quack.play();
                 display.value += 8;
                 break;
             case "9":
+                quack.play();
                 display.value += 9;
                 break;
             case "0":
+                quack.play();
                 display.value += 0;
                 break;
             case ".":
+                quack.play();
                 display.value += ".";
                 break;
             case "÷":
+                quack.play();
                 display.value += "÷";
                 break;
             case "-":
+                quack.play();
                 display.value += "-";
                 break;
             case "+":
+                quack.play();
                 display.value += "+";
                 break;
             case "x":
+                quack.play();
                 display.value += "x";
                 break;
         }
@@ -63,6 +81,7 @@ buttons.forEach(btn => {
 let evaluate = document.getElementById("equals");
 evaluate.addEventListener('click', function() {
     let stringToEvaluate = "";
+    quack.play();
 
     for (let char of display.value) {
         if (char === "÷") {
@@ -83,6 +102,7 @@ evaluate.addEventListener('click', function() {
 
 let sqrt = document.getElementById("sqrt");
 sqrt.addEventListener('click', function() {
+    quack.play();
     if (display.value === "" || isNaN(display.value)) {
         display.value = "undefined";
         return;
